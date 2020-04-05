ec9ca0217cec05e09ac5426b25b6de9ffa49b503
gowtham758550 <63159252+gowtham758550@users.noreply.github.com>
2020-04-05 16:18:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
+#import libraries smtplib and requests
 import smtplib
 import requests
+
+#get ip,location,city,hostname,region,country,postal and timezone of the machine where the code is run
+data = requests.get("https://ipinfo.io")
+msg = data.text
+
+sender = "python3testing755@gmail.com"
+password = "asdfghjkl1234567890"
+reciever = "gowtham758550@gmail.com"
+
+server = smtplib.SMTP('smtp.gmail.com',587)
+server.starttls()
+server.login(sender,password)
+
+server.sendmail(sender,reciever,msg)
+
 
 
